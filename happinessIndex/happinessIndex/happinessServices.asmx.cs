f5490b715b7050ce960ba0a042ac5fb6dda8243c
Jake Copeland <jrcopel1@asu.edu>
2019-04-09 10:38:53 -0700

    Added NLP library and function to query it. Have to finalize integration but getting close

--- happinessIndex/happinessIndex/happinessServices.asmx.cs
+++ happinessIndex/happinessIndex/happinessServices.asmx.cs
@@ -5,6 +5,9 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using java.util;
+using java.io;
+using edu.stanford.nlp.pipeline;
 using System.Web.Services;
 using Outlook = Microsoft.Office.Interop.Outlook;
 
@@ -533,6 +536,39 @@ namespace happinessIndex.App_Start
             {
                 reader.Close();
                 sqlConnection.Close();
+            }
+        }
+
+        [WebMethod]
+        public void SentimentAnalysis()
+        {
+            // Path to the folder with models extracted from `stanford-corenlp-3.7.0-models.jar`
+            var jarRoot = @"\Users\Jake\Documents\CIS440\OperationHappiness\happinessIndex\happinessIndex\stanford-corenlp-3.7.0-models\edu\stanford\nlp\models";
+
+            // Text for processing
+            var text = "Kosgi Santosh sent an email to Stanford University. He didn't get a reply.";
+
+            // Annotation pipeline configuration
+            var props = new Properties();
+            props.setProperty("annotators", "sentiment");
+            //props.setProperty("ner.useSUTime", "0");
+
+            // We should change current directory, so StanfordCoreNLP could find all the model files automatically
+            var curDir = Environment.CurrentDirectory;
+            Directory.SetCurrentDirectory(jarRoot);
+            var pipeline = new StanfordCoreNLP(props);
+            Directory.SetCurrentDirectory(curDir);
+
+            // Annotation
+            var annotation = new Annotation(text);
+            pipeline.annotate(annotation);
+
+            // Result - Pretty Print
+            using (var stream = new ByteArrayOutputStream())
+            {
+                pipeline.prettyPrint(annotation, new PrintWriter(stream));
+                System.Console.WriteLine(stream.toString());
+                stream.close();
             }
         }
     }
